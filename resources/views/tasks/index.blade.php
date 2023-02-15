0cd7c7c354274f818a2c7cf260322005c60bbb39
hajime.kimura <thehajimeeen@gmail.com>
2023-02-15 11:23:47 +0000

    CRUD task

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="prose ml-4">
+        <h2>タスク 一覧</h2>
+    </div>
+
+    @if (isset($tasks))
+        <table class="table table-zebra w-full my-4">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $tasks)
+                <tr>
+                    <td><a class="link link-hover text-info" href="{{ route('tasks.show', $tasks->id) }}">{{ $tasks->id }}</a></td>
+                    <td>{{ $tasks->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規タスクの作成</a>
+
+@endsection
